2395dcc714183c5ff0f95bcaa3f9d43702d846c0
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2025-02-15 23:53:40 -0700

    Add Hero section of home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,73 +1,118 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
+import { Image, StyleSheet, Platform,View, TextInput, Text, FlatList, ScrollView} from 'react-native';
+import { Colors } from '@/constants/Colors';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { Button, Card, IconButton } from 'react-native-paper';
+
+
+const data: Array<number> = [1,2,3,4,5 ,6]
 
 export default function HomeScreen() {
   return (
+    // picture and text
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          style={styles.reactLogo}
+          source={require('@/assets/images/homeImage.png')}
+          style={styles.homeImage}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      }
+      title="Welcome!"
+      subtitle="Share your expreriences, post questions and find support through a passionate community"
+    >
+
+      {/* search bar */}
+      <TextInput style={styles.keywordInput}>
+        Search for a keyword
+      </TextInput>
+
+      {/*filters  */}
+      <View style={styles.filterContainer}>
+        <IconButton icon="sort" />
+        <Button 
+          style={styles.filterButton} 
+          labelStyle={{padding: 0,position: 'absolute', left: "auto", margin: 0}}
+          contentStyle={{height: 25 }}
+          mode="outlined"
+          compact= {true}
+        >
+          <Text style={{fontSize: 9}}>Physical</Text>
+        </Button>
+        <Button 
+          labelStyle={{padding: 0,position: 'absolute', left: "auto", margin: 0}}
+          contentStyle={{height: 25 }}
+          style={styles.filterButton} 
+          mode='outlined'
+          compact= {true}
+        >
+          <Text style={{fontSize: 9}}>Emotional</Text>
+        </Button>
+        <Button 
+          labelStyle={{padding: 0,position: 'absolute', left: "auto", margin: 0}}
+          contentStyle={{height: 25 }}
+          style={styles.filterButton} 
+          mode='outlined'
+        >
+          <Text style={{fontSize: 9}}>Sexual violence</Text>
+        </Button>
+      </View  >
+      
+      <ScrollView>
+       {data.map((item, index) => {
+          return<View style={styles.postContainer} key={index}>
+            <ThemedText type="subtitle">Post {item}</ThemedText>
+          </View>
+        })}
+      </ScrollView>
+
+
+
+   </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
     gap: 8,
+  },
+  keywordInput: {
+    height: 30,
+    borderWidth: 1,
+    padding: 0,
+    paddingLeft: 20,
+    borderRadius: 15,
+    backgroundColor: Colors.light.background,
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: "5%"
+
+    
+
+  },
+  filterButton: {
+    width: "23%",
+  },
+  postContainer: {
+    height: 100,
+    width: "100%",
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
+  homeImage: {
+    height: "100%",
+    width:  "100%",
     bottom: 0,
     left: 0,
+    borderRadius: 10,
     position: 'absolute',
   },
 });
